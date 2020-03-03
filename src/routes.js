36011938545fafc9cb14a4085c78a37121b2fe5e
Elderson Gama <eldesondagama@hotmail.com>
2020-03-03 18:06:44 -0300

    Finish challenge

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,70 @@ const Router = require('express');
 
 const routes = new Router();
 
+// projects
+const projects = [];
+
+// middlewares to check if project exists
+function checkProjecExists(req, res, next) {
+
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project does not exists' });
+  }
+
+  return next();
+}
+
+// create projects
+routes.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+// show all projects
 routes.get('/projects', (req, res) => {
-  return res.json({ project: 'none' });
-})
+
+  if (projects < 1) {
+    return res.status(400).json({ error: 'No one project created!' });
+  }
+
+  return res.json(projects);
+});
+
+// show expecific project
+routes.get('/projects/:id', checkProjecExists, (req, res) => {
+  const { id } = req.params;
+
+  const index = projects.findIndex(p => p.id == id);
+
+  // if (!projects[index]) {
+  //   return res.status(400).json({ error: 'Project found!' });
+  // }
+
+  return res.json(projects[index]);
+});
+
+routes.delete('/projects/:id', checkProjecExists, (req, res) => {
+  const { id } = req.params;
+
+  const index = projects.findIndex(p => p.id == id);
+
+  projects.splice(index, 1);
+
+  return res.json({ success: 'Project deleted!' });
+});
+
 
 module.exports = routes;
